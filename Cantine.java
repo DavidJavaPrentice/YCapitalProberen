266461e863c31a57bbef4f610c96ffbe39ad59a9
David Prentice <davidjprentice152@gmail.com>
2019-01-23 13:20:07 +0100

    start

---
+++ Cantine.java
@@ -0,0 +1,23 @@
+package JavaBook;
+
+public class Cantine {
+
+}
+
+class Voedsel{}
+
+class Uiensoep extends Voedsel{}
+
+class Groentesoep extends Voedsel implements Vegetarisch{}
+
+class thaiRundSalade extends Voedsel{}
+
+class tonijnSalade extends Voedsel{}
+
+class geitenKaasSalade extends Voedsel implements Vegetarisch{}
+
+class broodjeKaas extends Voedsel implements Vegetarisch{}
+
+class broodjeHam extends Voedsel{}
+
+interface Vegetarisch{}
